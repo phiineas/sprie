e413c3ea14549fc839e41f117f699fcfe541f080
phiineas <geek.jainil18@gmail.com>
2025-07-10 18:05:07 +0530

    updated

---
+++ src/utils/levenshteinDistance.ts
@@ -0,0 +1,24 @@
+function levenshteinDistance(a: string, b: string): number {
+    const dp = Array(a.length + 1).fill(null).map(() => Array(b.length + 1).fill(0));
+
+    for (let i = 0; i <= a.length; i++) {
+        dp[i][0] = i;
+    }
+
+    for (let j = 0; j <= b.length; j++) {
+        dp[0][j] = j;
+    }
+
+    for (let i = 1; i <= a.length; i++) {
+        for (let j = 1; j <= b.length; j++) {
+            const cost = a[i - 1] === b[j - 1] ? 0 : 1;
+            dp[i][j] = Math.min(
+                dp[i - 1][j] + 1, 
+                dp[i][j - 1] + 1, 
+                dp[i - 1][j - 1] + cost
+            );
+        }
+    }
+
+    return dp[a.length][b.length];
+}
